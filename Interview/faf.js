27f6d33c8611f6f715b3937ab043a3f3aa3cd7b7
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-05 23:16:29 +0530

    hello

---
+++ Interview/faf.js
@@ -0,0 +1,18 @@
+// function divide(a,b) {
+//     return a/b
+// }
+// let d1 = divide(133,11)
+// console.log(d1);
+
+// function divide(a,b) {
+//     return 20/10
+// }
+// console.log(divide());
+
+//using FAF(Arrow-Function)
+let add = (a,b,c,d,e)=>{
+    return a+b+c+d+e
+}
+let a1= add(97,92,99,98,100)
+// let a1= add(100,100)
+console.log(a1);
