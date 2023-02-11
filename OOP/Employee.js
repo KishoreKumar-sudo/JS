4958ef46a81a042972aeb5ca4e84050cf5eff4d2
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-11 12:31:38 +0530

    employee

--- OOP/Employee.js
+++ OOP/Employee.js
@@ -1,4 +1,12 @@
 class Employee {
-
+    id=101;
+    name="Kishore";
+    email='kk1045@gmail.com'
+    get_Details(){
+        console.log('GM')
+    }
 }
-console.log(new Employee)
+let e1 = new Employee()
+// let e2 = new Employee();
+console.log(e1.id);
+// console.log(Employee.name)
