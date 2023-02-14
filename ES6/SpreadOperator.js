5fa0817f6350ae9df65914c61c077407ec0eb98c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-14 20:05:51 +0530

    timesaving

--- ES6/SpreadOperator.js
+++ ES6/SpreadOperator.js
@@ -1,6 +1,6 @@
-
+//Extract Array/Value/Object/Element & copy (i.e) Merging two arrays & extending ur array
 let a = [25, 45, 65]
 let b = [20, 40, 60]
-//Extract & copy 
+//Triple-Dot-Operator Applicable for both arrays & objects
 let c = [...a, ...b]
 console.log(c);
