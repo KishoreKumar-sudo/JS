f29364af08834e489c7100b8a219b58401316ba0
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-15 22:38:51 +0530

    tttt

--- ES6/Callback.js
+++ ES6/Callback.js
@@ -1,1 +1,11 @@
-()=>{}
+let sum = (a, b) => {
+    return a + b
+}
+let multi = () => {
+    return a * b
+}
+let calculate = (a, b, callback) => {
+    return (a, b)
+}
+let r1 = calculate(10, 20, sum)
+console.log(r1);
