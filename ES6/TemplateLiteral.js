a8390d21a2a422c978a6a1874f279424c017205f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-16 14:14:23 +0530

    tss

---
+++ ES6/TemplateLiteral.js
@@ -0,0 +1,6 @@
+let firstName = "Hema"
+let lastname = "Kishore"
+let city = "Krishnagiri"
+
+console.log(firstName + " " + lastname + " lives in " + city)
+console.log(firstName + lastname + " lives in " + city)
