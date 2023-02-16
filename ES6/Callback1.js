5107fac14886cc2054ad67ff74e28121e8001a8c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-16 13:50:56 +0530

    finish

--- ES6/Callback1.js
+++ ES6/Callback1.js
@@ -1,1 +1,9 @@
 //While invoking an function passing another function as an argument nothing but the callback 
+function add(a, b, c) {
+console.log(a);
+console.log(b);
+c()
+}
+add(10, 20, ()=>{
+    console.log("Hello, GN")
+})
