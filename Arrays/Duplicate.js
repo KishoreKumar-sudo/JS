1248bba5a939090dd568ecacb04129c7f013d96d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-02 23:19:27 +0530

    changes

--- Arrays/Duplicate.js
+++ Arrays/Duplicate.js
@@ -2,7 +2,7 @@ var arr =[2,3,5,7,11,5,13,7]
 for (a of arr){
     // console.log(arr.indexOf(a))
 }
-// console.log(arr.length);
+console.log(arr.length);
 
 function removeDuplicates() {
     return arr.filter((item, index) => arr.indexOf(item) === index)
