83dee7e2acf1554f0c83f74c13c8cd276459091c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-04 21:33:46 +0530

    finish

--- Arrays/Duplicate.js
+++ Arrays/Duplicate.js
@@ -9,8 +9,8 @@ function removeDuplicates() {
     return arr.filter((item, index) => arr.indexOf(item) === index)
 
 }
-// console.log(removeDuplicates(arr))
-// console.log(removeDuplicates(arr).length)
+console.log(removeDuplicates(arr))
+console.log(removeDuplicates(arr).length)
 
 //default method for removing duplicate
 let newArr = [...new Set(arr)]
