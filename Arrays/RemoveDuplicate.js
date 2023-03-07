5d784d6d16087021c47e6fd8cc6af4c0312898da
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-07 11:37:17 +0530

    finished

--- Arrays/RemoveDuplicate.js
+++ Arrays/RemoveDuplicate.js
@@ -1,4 +1,10 @@
 const names = ["rohit", "sky", "pollard", "ishan", "Rohit", "Pollard", "bumrah", "malinga"]
 function removeDuplicate(arr) {
-
+    let newArray =[];
+    for (let i = 0; i< array.length; i++) {
+            if(newArray.includes(arr[i].toLowerCase()))
+            newArray.push(arr[i].toLowerCase());
+    }
+    return newArray
 }
+console.log(removeDuplicate(names));
