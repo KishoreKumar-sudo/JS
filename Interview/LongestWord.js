32d0cafda20262b7dfead14513af16e719e378d1
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-09 22:49:27 +0530

    missing

--- Interview/LongestWord.js
+++ Interview/LongestWord.js
@@ -2,7 +2,7 @@ const languages = ["Java", "Python", "C", "Javascript", "Flutter", "Go"]
 function longestWord(arr) {
     let longWord = arr[0];
     for (const item of arr) {
-        if (item.length > longWord) {
+        if (item.length > longWord.length) {
             longWord = item;
         }
         return longWord;
