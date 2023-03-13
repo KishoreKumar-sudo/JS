2febd188b3bb343f37ec22af650aa4e50758646d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-13 23:14:08 +0530

    alignment

--- Interview/Boolean.js
+++ Interview/Boolean.js
@@ -1,5 +1,5 @@
-let a=6
-let b=typeof ( a== 6)
-let c=typeof ( a=== 6)
+let a = 6
+let b = typeof (a == 6) //Equality operator
+let c = typeof (a === 6) //Strictly(Triple)Equal operator
 console.log(b); //boolean
 console.log(c); //boolean
