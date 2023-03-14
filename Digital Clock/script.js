aedbd737ab64519c78e801053735a6686b3c1f45
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-14 20:23:21 +0530

    getting-id

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -3,5 +3,9 @@ function displayTime(){
     let hr = dateTime.getHours()
     let min = dateTime.getMinutes()
     let sec = dateTime.getSeconds()
+    document.getElementById('hours').innerHTML=hr
+    document.getElementById('minutes').innerHTML=min
+    document.getElementById('seconds').innerHTML=sec
+
 }
 setInterval(displayTime, 500)
