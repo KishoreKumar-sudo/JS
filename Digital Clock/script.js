84eba219110d0987fef20cb6532bd3af9ddc05ce
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-14 20:33:02 +0530

    if or else

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,11 +1,17 @@
+let ampm = document.getElementById('ampm')
 function displayTime(){
     let dateTime = new Date() 
     let hr = dateTime.getHours()
     let min = dateTime.getMinutes()
     let sec = dateTime.getSeconds()
+    if (hr>12){
+        hr = hr -12
+        ampm.innerHTML='PM'
+    }
     document.getElementById('hours').innerHTML=hr
     document.getElementById('minutes').innerHTML=min
     document.getElementById('seconds').innerHTML=sec
+
 
 }
 setInterval(displayTime, 500)
