aed57fc58cc0f32ae4983878e31e324c46f9b485
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-14 20:18:00 +0530

    setting interval

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,1 +1,7 @@
-let dateTime = new Date() 
+function displayTime(){
+    let dateTime = new Date() 
+    let hr = dateTime.getHours()
+    let min = dateTime.getMinutes()
+    let sec = dateTime.getSeconds()
+}
+setInterval(displayTime, 500)
