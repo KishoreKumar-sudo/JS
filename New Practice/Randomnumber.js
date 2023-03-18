62376725a9ca21f6a29118e7c983f756deea1e8d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-18 22:14:46 +0530

    explanation

--- New Practice/Randomnumber.js
+++ New Practice/Randomnumber.js
@@ -1,2 +1,4 @@
-const a =Math.random()
+//generating a number
+const a = Math.random()
 console.log(a)
+//Here, we have declared a variable a and assigned it a random number greater than or equal to 0 and less than 1.
