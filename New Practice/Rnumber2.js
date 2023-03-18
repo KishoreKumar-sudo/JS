a7e2afdb72971e1cb8f0d955bfde65bac522d083
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-18 22:20:16 +0530

    EOD

--- New Practice/Rnumber2.js
+++ New Practice/Rnumber2.js
@@ -1,5 +1,5 @@
 // generating an random number integer value b/w 1 & 10 
-const a =Math.floor(Math.random() * (10-1)) +1
+const a = Math.floor(Math.random() * (10 - 1)) + 1;
 console.log(`Random value between 1 and 10 is ${a}`);
 
 //This will show integer output between 1 (inclusive) to 10 (exclusive), i.e. (1 to 9). Here, Math.floor() is used to convert decimal to integer value.
