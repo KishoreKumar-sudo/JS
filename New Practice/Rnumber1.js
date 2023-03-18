6237a299e721cf8d45a0226bf080452917eee5ca
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-18 22:13:33 +0530

    explanation

--- New Practice/Rnumber1.js
+++ New Practice/Rnumber1.js
@@ -1,3 +1,6 @@
 // generating a random number b/w 
 const a = Math.random() * (10 - 1) + 1
 console.log(`Random value between 1 and 10 is ${a}`);
+
+//This will show a random floating-point number greater than 1 and less than 10.
+
