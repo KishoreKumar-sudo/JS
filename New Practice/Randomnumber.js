acefeb7ac52976d95cb3f04e49011ff08df5efa8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-18 22:19:51 +0530

    missing sc

--- New Practice/Randomnumber.js
+++ New Practice/Randomnumber.js
@@ -1,6 +1,6 @@
 //generating a number
-const a = Math.random()
-console.log(a)
+const a = Math.random();
+console.log(a);
 //Here, we have declared a variable a and assigned it a random number greater than or equal to 0 and less than 1.
 
 // Note: You might get a different output in the above program as Math.random() will generate a random number.
--- New Practice/Rnumber1.js
+++ New Practice/Rnumber1.js
@@ -2,7 +2,7 @@
 
 // We can use this value in the range (0,1) to find the random value between any two numbers using formula:
 //Math.random() * (highestNumber - lowestNumber) + lowestNumber
-const a = Math.random() * (10 - 1) + 1
+const a = Math.random() * (10 - 1) + 1;
 console.log(`Random value between 1 and 10 is ${a}`);
 
 //This will show a random floating-point number greater than 1 and less than 10.
