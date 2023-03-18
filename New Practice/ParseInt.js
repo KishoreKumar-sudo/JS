0df558aa8a03c77701532426269add3e352321a8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-18 15:02:01 +0530

    description

--- New Practice/ParseInt.js
+++ New Practice/ParseInt.js
@@ -1,3 +1,4 @@
-let x=10.5
-let y=parseInt(x) 
+// ParseInt is a pre-defined function which is convert (float value into integer) string to number and it doesn't supports decimal places.
+let x = 10.5
+let y = parseInt(x)
 console.log(y)
