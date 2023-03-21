26856b8eb51c689d2ec203f3d5157edd8adb2b61
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 11:49:13 +0530

    aligning

--- New Practice/GenerateOTP.js
+++ New Practice/GenerateOTP.js
@@ -1,10 +1,14 @@
- function generateOTP(){
-    let OTP=""
-     //Generating a 6-digit OTP
-     for (let i = 1; i <= 6; i++) {
-        OTP += Math.floor(Math.random() *10);
-     }
-     return OTP
- }
- // call the function to generate a 6-digit OTP
- console.log(generateOTP()); 
+function generateOTP() {
+    let OTP = "";
+    //Generating a 6-digit OTP
+    for (let i = 1; i <= 6; i++) {
+        OTP += Math.floor(Math.random() * 10);
+    }
+    return OTP;
+}
+// call the function to generate a 6-digit OTP
+console.log(generateOTP()); 
+console.log(generateOTP()); 
+console.log(generateOTP()); 
+console.log(generateOTP()); 
+console.log(generateOTP()); 
