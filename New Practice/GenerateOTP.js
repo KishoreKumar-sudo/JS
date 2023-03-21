6fb99d038455097affb49db1a2b95bc3b4c7343f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-21 11:45:47 +0530

    written code

--- New Practice/GenerateOTP.js
+++ New Practice/GenerateOTP.js
@@ -1,3 +1,8 @@
- function generateOTP{
-    
+ function generateOTP(){
+    let OTP=""
+     //Generating a 6-digit OTP
+     for (let i = 1; i <= 6; i++) {
+        OTP += Math.floor(Math.random() *10);
+        
+     }
  }
