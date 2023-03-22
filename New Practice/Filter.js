51af023364fd1f33bcbcfe2b00f2ce89b2ecc18c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-22 18:29:03 +0530

    alternate mtd worked

--- New Practice/Filter.js
+++ New Practice/Filter.js
@@ -1,6 +1,8 @@
-let x=[null,0,"0",false,"a"]
-// let y=x.filter((value)=> value)
-let y=x.filter(Boolean)
+let x = [null, 0, "0", false, "a"]
+let y=x.filter((value)=> value)
+console.log(y)
+//Another method
+//let y = x.filter(Boolean)
 console.log(y)
 
 // console.log(typeof null)
