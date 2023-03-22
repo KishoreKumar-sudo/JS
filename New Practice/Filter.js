7152c674e5bde2cae8020318bd41fc643f517bcf
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-22 18:30:49 +0530

    another clg & alignment

--- New Practice/Filter.js
+++ New Practice/Filter.js
@@ -1,8 +1,9 @@
 let x = [null, 0, "0", false, "a"]
-let y=x.filter((value)=> value)
+let y = x.filter((value) => value)
 console.log(y)
+
 //Another method
 //let y = x.filter(Boolean)
-console.log(y)
+// console.log(y)
 
 // console.log(typeof null)
