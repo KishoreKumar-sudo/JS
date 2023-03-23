022f70e3d3b4011652c4f42dc0133961ceb1a321
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-23 22:35:31 +0530

    clg done

--- New Practice/one.js
+++ New Practice/one.js
@@ -1,5 +1,5 @@
 let sentence = "Hello World"
 let text = "World Wide Web"
-/* if (sentence.includes(text.slice(0,5))) {
-    console.log();
-} */
+if (sentence.includes(text.slice(0,5))) {
+    console.log(Yes, Includes);
+}
