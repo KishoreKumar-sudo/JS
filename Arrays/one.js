321293707ad5842b4104fc757e1a49cc3b16e714
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-23 12:06:46 +0530

    array new file

--- Arrays/one.js
+++ Arrays/one.js
@@ -1,4 +1,8 @@
-const array = [1, 2, 3]
-const result = array[3]
-console.log(result);
-console.log(array);
+// const array = [1, 2, 3]
+// const result = array[3]
+// console.log(result);
+
+
+let array=[4,5,6]
+array[7]=7
+console.log(array[4]);
