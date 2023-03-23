996bf055c818c3de324bf50cd3e18cd19489b341
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-23 16:14:53 +0530

    created displayfood

--- Snake Game/App.js
+++ Snake Game/App.js
@@ -1,2 +1,26 @@
 const gameBoard = document.getElementById('gameBoard')
 const context = gameBoard.getContext('2d')
+
+const WIDTH = gameBoard.width;
+const HEIGHT = gameBoard.height;
+const UNIT = 25;
+
+
+startGame()
+
+function startGame() {
+    context.fillStyle = '#212121'
+    //for fillRectangle syntax would be (x-axis startingpt, y-axis startingpt, width, height) 
+    context.fillRect(0, 0, WIDTH, HEIGHT)
+    createFood();
+    displayFood();
+
+    function createFood() {
+
+    }
+
+    function displayFood() {
+        context.fillStyle = 'red'
+        context.fillRect = (foodX, foodY, UNIT, UNIT)
+    }
+}
