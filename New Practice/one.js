1207fce256b0740c624fd0d9c6fdbdbd4f2932f5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-23 22:37:09 +0530

    not included part in clg

--- New Practice/one.js
+++ New Practice/one.js
@@ -1,8 +1,8 @@
 let sentence = "Hello World"
 let text = "World Wide Web"
-if (sentence.includes(text.slice(0,5))) {
+if (sentence.includes(text.slice(0, 5))) {
     console.log(`Yes, Includes`);
 }
-else{(
-    
-)}
+else {
+    (console.log(`Not included`))
+}
