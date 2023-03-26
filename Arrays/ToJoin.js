9c8631faf23cf28d110a5f8c854aefdb6b7af3f0
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-26 22:35:41 +0530

    bikes varaibles created

--- Arrays/ToJoin.js
+++ Arrays/ToJoin.js
@@ -1,2 +1,4 @@
 //toStirng() returns a string with array values separated by commas and it doesnot changes the original array
 //syntax => array.toString()
+let bikes =["Yamaha","Bajaj","Honda","TVS"] 
+console.log(first)
