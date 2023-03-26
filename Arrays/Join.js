cb326b67dea55f53ecac299f094cf827c98bfa32
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-26 22:48:19 +0530

    FCOD

--- Arrays/Join.js
+++ Arrays/Join.js
@@ -1,7 +1,7 @@
 //Syntax => array.join(separator) 
 //the join() returns a new string by concatenating all of elements in an array, separted by commas or a specified saparator string.
 
-let bikes = ["Yamaha", "Bajaj", "Honda", "TVS"]
-console.log(bikes.join())
-console.log(bikes.join("")) //without spacing
+let bikes = ["Yamaha", "Bajaj", "Honda", "TVS"];
+console.log(bikes.join());
+console.log(bikes.join("")); //without spacing
 console.log(bikes.join("-"));
