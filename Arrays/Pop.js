22299fb4e611943da409856c93c89e05c280524e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-27 22:55:47 +0530

    FCOD

--- Arrays/Pop.js
+++ Arrays/Pop.js
@@ -2,4 +2,4 @@
 //the pop() removes the last element of an array & returns the removed element, this mtd changes the original array
 let bikes = ["Yamaha", "Bajaj", "Honda", "TVS"];
 console.log( bikes.pop());
-console.log(bikes);
+console.log(bikes);//changes the original array 
