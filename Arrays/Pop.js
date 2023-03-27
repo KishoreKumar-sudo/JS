a67017a260c7bee9d64afad9cdfa026acfcc30c7
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-27 22:52:52 +0530

    bikes variable created

--- Arrays/Pop.js
+++ Arrays/Pop.js
@@ -1,2 +1,3 @@
 // array.pop()=> syntax
-//the pop() removes the last element of an array & returns the removed element, this mtd changes the original array 
+//the pop() removes the last element of an array & returns the removed element, this mtd changes the original array
+let bikes = ["Yamaha", "Bajaj", "Honda", "TVS"];
