586fb2aa87fdf1c093364479a7d27f689b3bd794
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-28 23:15:41 +0530

    FCOD

--- Arrays/Push.js
+++ Arrays/Push.js
@@ -1,5 +1,6 @@
 // array.push(item1,item2,..., itemX)
 //the push() adds new items to the end of an array & changes the length of the array, returns the new length.
-let a=[1,2,3]
-a.push(a[2]++)
+let a = [1, 2, 3];
+a.push(a[2]++);
 console.log(a);
+//[1,2,4,3]
