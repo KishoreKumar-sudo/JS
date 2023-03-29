36778e037fb8a3c5b6c62a36e1d2cf465a079b7e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-29 23:01:45 +0530

    2nd o/p done

--- Arrays/Shift.js
+++ Arrays/Shift.js
@@ -1,4 +1,5 @@
 //syntax => array.shift()
 //shift() removes first element & returns it.
 let bikes=["Yamaha", "Bajaj", "Honda", "TVS", "Ducatti", "Royal Enfield"];
-console.log(bikes.shift())
+console.log(bikes.shift());
+console.log(bikes)
