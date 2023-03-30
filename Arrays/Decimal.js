fbbebda44f12838750df1e2f2c61d07d6f182a66
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 10:02:23 +0530

    commented the answers

--- Arrays/Decimal.js
+++ Arrays/Decimal.js
@@ -3,5 +3,5 @@ let decimal = 7.4
 let secDec = 7.5
 let getInt = Math.round(decimal)
 let secInt = Math.round(secDec)
-console.log(getInt)  
-console.log(secInt)  
+console.log(getInt) //returns 7
+console.log(secInt) //returns 8
