08bb620c050b17653092cd05ac0411c5dc21aec3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 10:00:59 +0530

    clg for decimal

--- Arrays/Decimal.js
+++ Arrays/Decimal.js
@@ -1,5 +1,7 @@
 // Math.round() in JS rounds a number to nearest integer
-let decimal=7.4
-let secDec=7.5
- let getInt=Math.round(decimal)
- let secInt=Math.round(secDec)
+let decimal = 7.4
+let secDec = 7.5
+let getInt = Math.round(decimal)
+let secInt = Math.round(secDec)
+console.log(getInt)  
+console.log(secInt)  
