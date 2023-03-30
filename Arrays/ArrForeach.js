cf16501caf47233b84a789e10f518219bbb20f48
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-30 10:20:26 +0530

    explanation given

--- Arrays/ArrForeach.js
+++ Arrays/ArrForeach.js
@@ -1,3 +1,5 @@
+//foreach() array mtd does not change anything in the original array & it does not return anything.
+//The multiplication is done & its result is discarded.  
 const arr = [1, 2, 3]
 arr.forEach(num => num * 2)
 console.log(arr);
