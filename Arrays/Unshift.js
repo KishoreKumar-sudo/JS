4d4a8083d7db4834b57017b0b46a1800667bbdec
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 23:08:50 +0530

    bikes array added

--- Arrays/Unshift.js
+++ Arrays/Unshift.js
@@ -1,2 +1,3 @@
 // syntax => array.unshift(item1, item2, ..., itemX )
 //dz mtd adds element to beginning & returns new array length
+let bikes = ["Yamaha", "Bajaj", "Honda", "TVS", "Ducatti", "Royal Enfield"];
