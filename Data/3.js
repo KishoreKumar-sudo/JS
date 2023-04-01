55a10796c181a8becf2de17f1c7b44ce84ee3f22
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 12:10:21 +0530

    explanation added

--- Data/3.js
+++ Data/3.js
@@ -1,4 +1,5 @@
-let button=document.querySelector("button")
-button.addEventListener("click", ()=>{
+//By passing the {once:true} option as the 3rd argument, we ensure that event listener will be triggered only once.
+let button = document.querySelector("button")
+button.addEventListener("click", () => {
     console.log("Button Clicked!");
-},{once :true})
+}, { once: true })
