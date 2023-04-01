4e4a38593435ca915bb6b865bac0dac3e5b1fc7e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 23:17:01 +0530

    FCOD

--- Arrays/DeleteOperator.js
+++ Arrays/DeleteOperator.js
@@ -1,4 +1,5 @@
 // syntax => delete array[index];
 //Array elements can be deleted using the JS opeartor delete & using delete leaves undefined holes in the array
- let fruits =["Apple", "Banana", "Grapes"]
- 
+let fruits = ["Apple", "Banana", "Grapes"]
+delete fruits[2];
+console.log(fruits);// ['Apple', 'Banana', <1 empty item> ]
