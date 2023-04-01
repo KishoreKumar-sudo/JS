5f902df2498b3092c17b3f1a2926a64b5956d07f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 23:15:08 +0530

    fruits assigned to variable

--- Arrays/DeleteOperator.js
+++ Arrays/DeleteOperator.js
@@ -1,3 +1,4 @@
 // syntax => delete array[index];
 //Array elements can be deleted using the JS opeartor delete & using delete leaves undefined holes in the array
+ let fruits =["Apple", "Banana", "Grapes"]
  
