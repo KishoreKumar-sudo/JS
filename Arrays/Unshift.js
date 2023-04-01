d5661f800c87598375dec4aab5dd0947a1b32926
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 23:09:58 +0530

    1st clg done

--- Arrays/Unshift.js
+++ Arrays/Unshift.js
@@ -1,3 +1,4 @@
 // syntax => array.unshift(item1, item2, ..., itemX )
 //dz mtd adds element to beginning & returns new array length
 let bikes = ["Yamaha", "Bajaj", "Honda", "TVS", "Ducatti", "Royal Enfield"];
+console.log(bikes.unshift("BMW", "Kawasaki"))
