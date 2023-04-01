00cfab0790873a5bfca04067e075c418eab96f25
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-01 11:36:17 +0530

    Alignment

--- ES6/Foreach.js
+++ ES6/Foreach.js
@@ -1,12 +1,12 @@
-let marks = [34,35,36,37,38]
-marks.forEach((mark)=>{
+let marks = [34, 35, 36, 37, 38];
+marks.forEach((mark) => {
     console.log(mark);
-    return + 1 
-})
+    return + 1
+});
 
-let x=[1,2,3,5]
-x.forEach((e)=>{
-    if (e>2 && e<5) {
+let x = [1, 2, 3, 5];
+x.forEach((e) => {
+    if (e > 2 && e < 5) {
         console.log(e);
     }
-})
+});
