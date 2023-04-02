2ec18af2912ba92e6a368261892c39a6c56f5337
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-02 17:02:20 +0530

    variables assigned & clg done

--- Arrays/Sorting.js
+++ Arrays/Sorting.js
@@ -1,3 +1,9 @@
 //syntax => array.sort(compareFunction)
 //dz mtd sorts the elements of an array, the elements as strings in alphabetical and ascending order & overwrites the original array.
+//sort() takes an "optional compare function"
  
+let array=[99,88,77,66,55]
+let string=["Zebra","Year","Van","Apple","Calendar"]
+
+array.sort();
+console.log(array)
