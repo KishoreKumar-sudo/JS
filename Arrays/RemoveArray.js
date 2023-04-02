e0f6596f5fe1ba49a5de8593d07dbd56352b8901
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-02 17:46:49 +0530

    o/p executed

--- Arrays/RemoveArray.js
+++ Arrays/RemoveArray.js
@@ -1,4 +1,5 @@
 //use the "Set" object in JS to remove duplicates from an array in just one line of code
 
 const arr=[1,2,3,4,4,2,1]
-
+const uniqueArr=[...new Set(arr)]
+console.log(uniqueArr); //[ 1, 2, 3, 4 ]
