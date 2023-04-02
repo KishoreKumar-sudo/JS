b2a3bcdada07076a9e041baa2c5f082ad7fb2d4d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-02 13:46:44 +0530

    syntax & explained

---
+++ Arrays/Concat.js
@@ -0,0 +1,2 @@
+// syntax => array1.concat(array2, array3, ..., arrayX) it doesn't change existing array
+//dz mtd concatenates (joins) two or more arrays, returns a new array & containing thw joined array 
