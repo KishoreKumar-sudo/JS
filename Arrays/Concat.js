19ac3d077a820c99e72168c6bdecf0ac3031c77d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-02 13:48:13 +0530

    3 variables assigned

--- Arrays/Concat.js
+++ Arrays/Concat.js
@@ -1,2 +1,5 @@
 // syntax => array1.concat(array2, array3, ..., arrayX) it doesn't change existing array
 //dz mtd concatenates (joins) two or more arrays, returns a new array & containing thw joined array 
+let array1 = [1,2,3]
+let array2 = [22,45,64]
+let array3=[122, 13]
