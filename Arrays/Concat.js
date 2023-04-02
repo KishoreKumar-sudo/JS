15837ce74bba45c2bf812581f19c631c0dc7b683
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-02 13:50:03 +0530

    assigned & clg done

--- Arrays/Concat.js
+++ Arrays/Concat.js
@@ -1,5 +1,10 @@
 // syntax => array1.concat(array2, array3, ..., arrayX) it doesn't change existing array
 //dz mtd concatenates (joins) two or more arrays, returns a new array & containing thw joined array 
-let array1 = [1,2,3]
-let array2 = [22,45,64]
-let array3=[122, 13]
+
+let array1 = [1, 2, 3]
+let array2 = [22, 45, 64]
+let array3 = [122, 13]
+
+let array_new = array1.concat(array2, array3);
+
+console.log(array_new);
