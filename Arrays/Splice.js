35cdca3e84094285e7346f2b5b5f0544f45c922e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-03 22:24:29 +0530

    FCOD

--- Arrays/Splice.js
+++ Arrays/Splice.js
@@ -1,6 +1,6 @@
 // syntax => array.splice(index, howmany, item1, ....., itemX)
 // dz mtd adds and/or removes the array elements, overwrites the original array.
- 
-let array=[99,32,23,46,56,100]
-array.splice(2,3,"a","b","c")
-console.log(array)
+
+let array = [99, 32, 23, 46, 56, 100]
+array.splice(2, 3, "a", "b", "c")
+console.log(array) //[ 99, 32, 'a', 'b', 'c', 100 ]
