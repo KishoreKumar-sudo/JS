a11b546ef063a4b277fb69ff47598538750fea30
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-03 22:21:28 +0530

    variables assigned

--- Arrays/Splice.js
+++ Arrays/Splice.js
@@ -1,2 +1,5 @@
 // syntax => array.splice(index, howmany, item1, ....., itemX)
 // dz mtd adds and/or removes the array elements, overwrites the original array.
+ 
+let array=[99,32,23,46,56,100]
+array.splice()
