62e29b4d4de3dc265f6ed02de059b3a56a97a4bc
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-05 23:11:22 +0530

    FCOD

--- Arrays/Slice.js
+++ Arrays/Slice.js
@@ -2,4 +2,4 @@
 //dz mtd slices out a piece from an array, it creates a new array
 let numbers = [1, 2, 3, 4, 5, 6]
 let num2 = numbers.slice(1, 4)
-console.log(num2);
+console.log(num2); //[ 2, 3, 4 ]
