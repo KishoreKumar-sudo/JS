17a2d5f48f61f463d9623f85de3487acd3aff76d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:49:51 +0530

    var assigned

--- Arrays/IndexOf.js
+++ Arrays/IndexOf.js
@@ -2,3 +2,5 @@
 
 //the indexOf() mtd returns the 1st index(position) of a specified value, returns -1 if the value is not found & it searches from left to right
 //negative start values counts from the last element(bt still reaches from left to right)
+
+let elements = ["Laptop", "Mobile", "Headset", "Router", "Modem"]
