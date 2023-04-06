aa22828df491e38793a046bed865f5e63fb8bf64
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 22:25:24 +0530

    spacing

--- Arrays/Decimal.js
+++ Arrays/Decimal.js
@@ -1,7 +1,10 @@
 // Math.round() in JS rounds a number to nearest integer
+
 let decimal = 7.4
 let secDec = 7.5
+
 let getInt = Math.round(decimal)
 let secInt = Math.round(secDec)
+
 console.log(getInt) //returns 7
 console.log(secInt) //returns 8
