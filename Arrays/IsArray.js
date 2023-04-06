f87445b817834ad69477527b35751aaa143029de
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:36:37 +0530

    variables assigned

--- Arrays/IsArray.js
+++ Arrays/IsArray.js
@@ -1,2 +1,5 @@
 // syntax Array.isArray(obj)
 //isArray() mtd returns true if an object is an array, otherwise false & check if an object is an array
+
+let numbers = [1, 2, 3, 4, 5, 6]
+let string ="CodeWithKishore"
--- Arrays/Reverse.js
+++ Arrays/Reverse.js
@@ -1,8 +1,8 @@
 // syntax => array.reverse()
 //dz mtd reverses the order of the element in the array, overwrites the original array
 
-let numbers=[1,2,3,4,5,6]
-let string=["A","B","C","D","E"]
+let numbers = [1, 2, 3, 4, 5, 6]
+let string = ["A", "B", "C", "D", "E"]
 
 // Reverses the elements in an array in place. This method mutates the array and returns a reference to the same array
 numbers.reverse()
