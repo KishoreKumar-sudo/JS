a910f1b760bdc4d5cce6cc794a589723ae8f86d3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 22:27:10 +0530

    equal spacing

--- Arrays/ArrForeach.js
+++ Arrays/ArrForeach.js
@@ -1,8 +1,7 @@
 //foreach() array mtd does not change anything in the original array & it does not return anything.
 //The multiplication is done & its result is discarded.  
+
 const arr = [1, 2, 3]
 arr.forEach(num => num * 2)
-console.log(arr);
 
-let x
-console.log(x) //undefined
+console.log(arr);
--- Arrays/DeleteOperator.js
+++ Arrays/DeleteOperator.js
@@ -1,8 +1,10 @@
 // syntax => delete array[index];
 //Array elements can be deleted using the JS opeartor delete & using delete leaves undefined holes in the array
+
 let fruits = ["Apple", "Banana", "Grapes"]
 //             [0]      [1]        [2]
 delete fruits[2];
+
 console.log(fruits);// ['Apple', 'Banana', <1 empty item> ]
 
 let x;
