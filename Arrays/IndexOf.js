8d5d6acc103851487d7a37d7de9b955f0da0b060
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:48:24 +0530

    small explanation

--- Arrays/IndexOf.js
+++ Arrays/IndexOf.js
@@ -1,2 +1,4 @@
 //syntax => array.indexOf(item, start)
+
 //the indexOf() mtd returns the 1st index(position) of a specified value, returns -1 if the value is not found & it searches from left to right
+//negative start values counts from the last element(bt still reaches from left to right)
