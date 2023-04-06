a654be2b71364df41d3c74f6fd9368a77c6807c9
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:25:16 +0530

    str assigned

--- Arrays/Reverse.js
+++ Arrays/Reverse.js
@@ -1,4 +1,4 @@
 // syntax => array.reverse()
 //dz mtd reverses the order of the element in the array, overwrites the original array
-let numbers=[1,2,3,4,5,6
-]
+let numbers=[1,2,3,4,5,6]
+let string=["A","B","C","D","E"]
