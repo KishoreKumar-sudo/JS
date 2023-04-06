5f32b65e29676d34417cb088a3631a910117d41d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:28:46 +0530

    aligned

--- Arrays/Reverse.js
+++ Arrays/Reverse.js
@@ -1,10 +1,12 @@
 // syntax => array.reverse()
 //dz mtd reverses the order of the element in the array, overwrites the original array
+
 let numbers=[1,2,3,4,5,6]
 let string=["A","B","C","D","E"]
+
 // Reverses the elements in an array in place. This method mutates the array and returns a reference to the same array
 numbers.reverse()
 console.log(numbers);//[ 6, 5, 4, 3, 2, 1 ]
 
 string.reverse()
-console.log(string);//
+console.log(string);//[ 'E', 'D', 'C', 'B', 'A' ]
