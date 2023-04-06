c973e700a8afdce949c8a878b4124cb087e8b68e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:37:26 +0530

    log

--- Arrays/IsArray.js
+++ Arrays/IsArray.js
@@ -2,4 +2,5 @@
 //isArray() mtd returns true if an object is an array, otherwise false & check if an object is an array
 
 let numbers = [1, 2, 3, 4, 5, 6]
-let string ="CodeWithKishore"
+let string = "CodeWithKishore"
+console.log(Array.isArray(numbers));
