1bb9e94e55bec36e7c118ed74746575311041982
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 22:29:13 +0530

    FCOD

--- Arrays/Duplicate.js
+++ Arrays/Duplicate.js
@@ -1,4 +1,16 @@
-var arr =[2,3,5,7,11,5,13,7]
+var arr = [2, 3, 5, 7, 11, 5, 13, 7]
+
+function removeDuplicates() {
+    return arr.filter((item, index) => arr.indexOf(item) === index)
+
+}
+
+console.log(removeDuplicates(arr))
+console.log(removeDuplicates(arr).length)
+
+//default method for removing duplicate
+let newArr = [...new Set(arr)]
+console.log(newArr)
 
 //this method is not applicable
 /* for (a of arr){
@@ -6,13 +18,3 @@ var arr =[2,3,5,7,11,5,13,7]
 }
 console.log(arr.length); */
 
-function removeDuplicates() {
-    return arr.filter((item, index) => arr.indexOf(item) === index)
-
-}
-console.log(removeDuplicates(arr))
-console.log(removeDuplicates(arr).length)
-
-//default method for removing duplicate
-let newArr = [...new Set(arr)]
-console.log(newArr) 
