5c69b59b48f99724b93fa902bbdb317a85962c57
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-06 21:27:57 +0530

    log2

--- Arrays/Reverse.js
+++ Arrays/Reverse.js
@@ -4,4 +4,7 @@ let numbers=[1,2,3,4,5,6]
 let string=["A","B","C","D","E"]
 // Reverses the elements in an array in place. This method mutates the array and returns a reference to the same array
 numbers.reverse()
-console.log(numbers);
+console.log(numbers);//[ 6, 5, 4, 3, 2, 1 ]
+
+string.reverse()
+console.log(string);//
