33487e26db9e38aaabe68b979c8423d3837c9239
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-07 12:32:19 +0530

    for loop

--- New Practice/4.js
+++ New Practice/4.js
@@ -1,5 +1,10 @@
+var i
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000)
+}
+console.log("first", i)
 
 
-
-
-console.log(NaN=NaN)
+console.log(NaN = NaN)
