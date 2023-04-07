dee1dce32dab6a275351015ebed8a39b37c433b3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-07 15:37:37 +0530

    3

--- New Practice/3.js
+++ New Practice/3.js
@@ -1,3 +1,13 @@
 let x = 10;
 let y = 20;
-console.log("total = " + x + y);
+console.log("total = " + x + y); //total = 1020
+
+var i
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000)
+}
+console.log("first", i)
+
+console.log(NaN = NaN)
