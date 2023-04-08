93382ee2c2f5f0769c8e3ef1822a0197c990e3a3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-08 22:50:06 +0530

    7

--- New Practice/Object.js
+++ New Practice/Object.js
@@ -4,10 +4,13 @@ const obj3 = { a: 20, b: { x: 80, y: 60 } }
 function compare(obj1, obj2) {
     for (let key in obj1) {
         if (typeof obj1[key] === "object") {
-            return compare(obj1[key],obj2[key]);
+            return compare(obj1[key], obj2[key]);
         }
-        else{
-            if(obj1[key]===obj2[key])
+        else {
+            if (obj1[key] === obj2[key]) {
+                return false
+            }
         }
     }
+    return true
 }
