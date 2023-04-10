0f035512bb5b7f267b13ab173a62759758ca8681
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-10 18:01:18 +0530

    3

--- New Practice/Function.js
+++ New Practice/Function.js
@@ -1,6 +1,7 @@
-const func =function(a,b){
+const func = function (a, b) {
     return arguments.length
 }
-const result=func(4,3,2,1)
-alert(result)
-console.log(alert);
+const result = func(4, 3, 2, 1)
+// alert(result)
+console.log(result); //4
+
