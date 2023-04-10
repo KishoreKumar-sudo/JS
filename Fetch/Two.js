ab388c4c93b820d64daf8db6034f3f1000cd63ef
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-10 18:10:33 +0530

    6

---
+++ Fetch/Two.js
@@ -0,0 +1,7 @@
+let x = true + true
+let y = x + false
+console.log(y) //2
+//when we perform arithmetic operation with true, false statement & it returns value for true=> 1, false=> 0 So ,
+// x = 1 + 1 => 2
+// y = 2 + 0 => 2
+
