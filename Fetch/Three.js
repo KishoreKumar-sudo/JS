9fd6ae29889771fa0706dfba08d9626d127eccc4
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-10 18:21:18 +0530

    10

--- Fetch/Three.js
+++ Fetch/Three.js
@@ -2,6 +2,6 @@
 const [x, setX] = useState({
     a: 10,
     b: 30
-})
+}) //o/p not found
 //x.a=20;x.b=40
 //setX({a:20,b:40})
--- New Practice/NumberIsInteger.js
+++ New Practice/NumberIsInteger.js
@@ -2,5 +2,17 @@ const value = 20.0
 const result = Number.isInteger(value)
 console.log(result); //true
 
-// incorrect variable =$results
-//which mt returns a new array with selected elements? filter()
+//which of the following is incorrect variable ?
+// 1r2r3r3urtu
+// _2results
+// 2results
+// $results
+
+// Ans => $results
+
+//which mt returns a new array with selected elements?
+// filter()
+// map()
+// reduce()
+
+// Ans => filter()
