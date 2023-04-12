2b17e50b7fb81833f087c3ce480807d06bdb1631
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-12 23:07:15 +0530

    6

--- ES6/SetTimeout.js
+++ ES6/SetTimeout.js
@@ -6,7 +6,13 @@ console.log(2);
 var i
 for (let i = 0; i < 3; i++) {
     setTimeout(() => {
-        console.log(i)
-    }, 1000);
+        console.log(i);
+    }, 1000)
 }
-console.log("first", i);
+//first undefined
+// NaN
+// 0
+// 1
+// 2
+
+console.log("first", i)
--- New Practice/3.js
+++ New Practice/3.js
@@ -2,18 +2,4 @@ let x = 10;
 let y = 20;
 console.log("total = " + x + y); //total = 1020
 
-var i
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => {
-        console.log(i);
-    }, 1000)
-}
-//first undefined
-// NaN
-// 0
-// 1
-// 2
-
-console.log("first", i)
-
 console.log(NaN = NaN)
