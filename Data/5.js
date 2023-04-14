6004ee76b16d18f62618617e4cff7bb453b14dd8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-14 23:08:55 +0530

    13

--- Data/5.js
+++ Data/5.js
@@ -12,5 +12,8 @@ b = 30
 
 console.log(true + false); //(1 + 0 => 1)
 
+//A value between 2 and 36 that specifies the base of the number in string. If this argument is not supplied, strings with a prefix of '0x' are considered hexadecimal
+// All other strings are considered decimal.
 const num = parseInt('7*6', 10)
+
 console.log(num); //7
