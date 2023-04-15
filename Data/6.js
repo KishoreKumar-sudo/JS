c046e7f3b3c956f31d87a407b562aad2df6c7b89
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-15 22:07:45 +0530

    5

--- Data/6.js
+++ Data/6.js
@@ -3,5 +3,6 @@ let y = 2
 console.log(x == y); //true
 console.log(x === y); //false
 
+//this is much confused question for beginners, in JS there is no array type data & even a function itself is an object.
 let z = [1, 2, 3]
 console.log(typeof z); //object
