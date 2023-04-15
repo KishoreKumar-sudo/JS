62a3faf0606119741c03da14ecbd240c24b6c7c5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-15 22:05:04 +0530

    4

--- Data/6.js
+++ Data/6.js
@@ -1,7 +1,7 @@
 let x = [2]
 let y = 2
-console.log(x == y);
-console.log(x === y);
+console.log(x == y); //true
+console.log(x === y); //false
 
 let z = [1, 2, 3]
-console.log(typeof z);
+console.log(typeof z); //object
