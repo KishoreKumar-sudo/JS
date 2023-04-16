fca4f6e4856de585695aece26ad8851789512c2e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-16 22:27:31 +0530

    3

--- Data/6.js
+++ Data/6.js
@@ -10,5 +10,3 @@ console.log(typeof z); //object
  //object! => when an array is created in JS, it's actually an object with integer-based keys that serve as the index of array elements.
 //the array elements are stored as properties of dz object, with their corresponding index numbers as the property names.
 
-// what is the purpose of a JS function?
-//To Perform Task/Calculation
