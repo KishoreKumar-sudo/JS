26ab6a820c23477906232dd268dcdac2aa970118
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-16 22:39:17 +0530

    7

--- Arrays/Slice.js
+++ Arrays/Slice.js
@@ -1,11 +1,12 @@
 // Syntax array.slice(start, end);
 //dz mtd slices out a piece from an array, it creates a new array
 
-let number = [1, 2, 3, 4, 5, 6]
+let number = [1, 2, 3, 4, 5, 6];
 let number1 = number.slice(1, 4)
 
 console.log(number1); //[ 2, 3, 4 ]
 
-const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5];
 const result = arr.slice(-3)
-console.log(result);
+
+console.log(result); //[ 3, 4, 5 ]
