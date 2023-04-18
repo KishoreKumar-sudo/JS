b035f7ce1cb2180db46c3e110f6e3b2d7fea6c1c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-18 22:34:23 +0530

    4

--- Arrays/Slice.js
+++ Arrays/Slice.js
@@ -6,6 +6,9 @@ let number1 = number.slice(1, 4)
 
 console.log(number1); //[ 2, 3, 4 ]
 
+//slice() mtd returns a new array containing the elements of original array starting from the specified start index,
+//gng upto(nt including) the specified end index. if the start index is negative, it counts from the end of array.
+
 const arr = [1, 2, 3, 4, 5];
 const result = arr.slice(-3)
 
