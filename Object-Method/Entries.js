97e96cb6433c0ad94f5ca4185b74efc8b1bd5beb
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-19 22:59:22 +0530

    6

--- Object-Method/Entries.js
+++ Object-Method/Entries.js
@@ -1,6 +1,11 @@
 // Takes an object & returns its own enumberable string - keyed property[key, value] pairs of the object
 
-const drinks={
-    maple:'out of stack',
-    orange:3.5
+const drinks = {
+    maple: 'out of stack',
+    orange: 3.5
+}
+
+for (const [name, cost] of 
+Object.entries(drinks)) {
+    console.log(`$`);
 }
