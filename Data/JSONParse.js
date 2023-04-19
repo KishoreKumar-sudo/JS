ec884e9368da85ba709f56c52a5d12ca024c0cd9
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-19 22:51:25 +0530

    5

--- Data/JSONParse.js
+++ Data/JSONParse.js
@@ -1,1 +1,7 @@
-// JSON.parse() converts a JSON string to a JS object 
+// JSON.parse() converts a JSON string to a JS object
+
+let userInfoStr = `{ "name": "Ryan", "country": "UAE" }`
+let userInfoObj = JSON.parse(userInfoStr)
+
+//Converts to JS object
+console.log(userInfoObj) //{"name":"Ryan","country":"UAE"}
