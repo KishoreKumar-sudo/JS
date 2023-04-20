7af60fb50ce8bbbe02743b45e33760cfae5b5bac
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 23:18:14 +0530

    29

--- Arrays/Entries.js
+++ Arrays/Entries.js
@@ -1,4 +1,10 @@
 //entries() returns an Array Iterator object with key/value pairs & dz mtd doesn't change the original array.
 
+//syntax => array.entries()\
+
 const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
 const day = days.entries()
+
+for (let x of day) {
+    console.log(x + "/n");
+}
