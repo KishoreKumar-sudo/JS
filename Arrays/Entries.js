c20c5a5e05dd343c58f69983a31cb28ef538917f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 23:22:09 +0530

    30

--- Arrays/Entries.js
+++ Arrays/Entries.js
@@ -5,6 +5,18 @@
 const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
 const day = days.entries()
 
+//for of => const iterator of object
+
 for (let x of day) {
-    console.log(x + "/n");
+    console.log(x + "\n");
 }
+
+/*  Expeected O / P:
+0, sun
+1, mon
+2, tue
+3, wed
+4, thu
+5, fri
+6, sat */
+
