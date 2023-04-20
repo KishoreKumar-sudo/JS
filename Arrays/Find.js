924453e49bb00e6ca488775977da6f42c760653e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 22:47:05 +0530

    11

--- Arrays/Find.js
+++ Arrays/Find.js
@@ -1,4 +1,6 @@
 // syntax(arrow function) => find((element)=>{/* ... */})
 
-const array=[5,12,8,130,44]
-const found=array.find((element)=> element>10)
+const array = [5, 12, 8, 130, 44]
+const found = array.find((element) => element > 10)
+
+console.log(found); //12
