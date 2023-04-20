7e869e468a04782105d134909d67c54c7a6df796
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 22:31:35 +0530

    3

--- Arrays/LastIndexof.js
+++ Arrays/LastIndexof.js
@@ -1,4 +1,6 @@
 // syntax => array.lastIndexOf(item, start);
 
 let elements = ["Laptop", "Headset", "Mobile", "Router", "Mobile"]
-console.log(elements.indexOf("Mobile", 0)); 
+
+console.log(elements.indexOf("Mobile", 0)); //2
+console.log(elements.lastIndexOf("Mobile", 4)); //4
