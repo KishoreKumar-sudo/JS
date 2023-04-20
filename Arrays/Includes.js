729d59bb4c264594e23ce95769fcb76fa8ecf369
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 22:56:41 +0530

    14

--- Arrays/Includes.js
+++ Arrays/Includes.js
@@ -1,2 +1,4 @@
 // includes() returns an true if an array contains a specified value(case sensitive).
-//if the value is not found returns false 
+//if the value is not found returns false
+
+// syntax => array.includes(element, start)
