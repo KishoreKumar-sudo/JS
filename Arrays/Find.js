923a3f6b2bdec33e5b2ccb79de03902a2f71488e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-20 22:53:21 +0530

    11

--- Arrays/Find.js
+++ Arrays/Find.js
@@ -1,5 +1,6 @@
+//find() returns first element in the provided array that satisfies provided testing function
+// 
 // syntax(arrow function) => find((element)=>{/* ... */})
-
 const array = [5, 12, 8, 130, 44]
 const found = array.find((element) => element > 10)
 
