6c25e8a28f5f1314425dc86cb734fa14b1610a3b
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-21 22:32:06 +0530

    6

--- Arrays-Inbuilt-Mtd/Every.js
+++ Arrays-Inbuilt-Mtd/Every.js
@@ -1,12 +1,12 @@
-// syntax => every((element))=>{/* code */})
-
 //every() mtd tests whether all elements in array pass the test implemented by the provided function.
 //it returns a boolean value
 
+// syntax => every((element))=>{/* code */})
 const array1 = [1, 30, 39, 29, 10, 13];
 const isBelow_1 = (currentValue) => currentValue < 40;
 
 console.log(array1.every(isBelow_1)); //true
 
 const isBelow_2 = (currentValue) => currentValue < 30;
-console.log(array1.every(isBelow_2));
+
+console.log(array1.every(isBelow_2)); //false
---
+++ Arrays-Inbuilt-Mtd/Filter.js
@@ -0,0 +1,4 @@
+//syntax => array.filter(function(currentValue, index, arr), thisValue)
+
+//filter() creates a new array filled with elements that pass a test provided by a function & dz mtd does not change the original array
+
