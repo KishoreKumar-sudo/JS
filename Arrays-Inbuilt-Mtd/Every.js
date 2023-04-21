ba64917d1a19c94d395c69a3c926ae6672e3f6ad
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-21 22:22:35 +0530

    4

--- Arrays-Inbuilt-Mtd/Every.js
+++ Arrays-Inbuilt-Mtd/Every.js
@@ -3,3 +3,5 @@
 //every() mtd tests whether all elements in array pass the test implemented by the provided function.
 //it returns a boolean value
 
+const array1=[1,30,39,29,10,13];
+const isBelow_1=(currentValue)=>currentValue<40;
