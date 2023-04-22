1f34fa093f1cbb91ddba03ed310c3e721fa111c8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-22 22:42:22 +0530

    7

--- Arrays-Inbuilt-Mtd/CopyWithin.js
+++ Arrays-Inbuilt-Mtd/CopyWithin.js
@@ -4,3 +4,9 @@
 //it doesn't add items to the array
 
 //copy the frst two array elements to the last two array elements
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"]
+fruits.copyWithin(2, 0)
+console.log(fruits);
+
+const vegetables = ["Beetroot", "Carrot", "Onion", "Tomato","",""]
