24b42280c86ef3359ce87e4296731c35078eb39c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-22 22:15:40 +0530

    3

--- Arrays-Inbuilt-Mtd/Fill.js
+++ Arrays-Inbuilt-Mtd/Fill.js
@@ -4,3 +4,7 @@
 //start & end position can be specified, if nt all elements will be filled 
 
 //Fill alll the elements with a value
+
+const fruits=["Banana","Orange","Apple","Mango"]
+fruits.fill("Kiwi")
+console.log(fruits) //[ 'Kiwi', 'Kiwi', 'Kiwi', 'Kiwi' ]
