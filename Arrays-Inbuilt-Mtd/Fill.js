3f338085b11c1ad15001551c948200e6c3b7ee59
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-22 22:11:37 +0530

    2

--- Arrays-Inbuilt-Mtd/Fill.js
+++ Arrays-Inbuilt-Mtd/Fill.js
@@ -1,3 +1,6 @@
 // syntax => array.fill(value, start, end)
 
-//fill() mtd 
+//fill() mtd fills specified elements in an array with a value mtd overwrites the original array
+//start & end position can be specified, if nt all elements will be filled 
+
+//Fill alll the elements with a value
