1605e9a3147c3370105a80d1ff950f6bc0babf83
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-22 22:36:17 +0530

    6

--- Arrays-Inbuilt-Mtd/CopyWithin.js
+++ Arrays-Inbuilt-Mtd/CopyWithin.js
@@ -1,3 +1,6 @@
 // syntax => array.copyWithin(target, start, end)
 
-//copywithin() mtd
+//copywithin() mtd copies array elements to another position in the array, dz mtd overwrites existing values
+//it doesn't add items to the array
+
+//copy the frst two array elements to the last two array elements
