15858343f294fa48c6ede3aaefa51745e9f9ec01
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-23 20:46:48 +0530

    3

--- Arrays-Inbuilt-Mtd/Some.js
+++ Arrays-Inbuilt-Mtd/Some.js
@@ -1,4 +1,12 @@
 //syntax => array.some(function(value, index, arr),this)
 
 //some() tests whether atleast one element in the array passes test implemented by provided function
-// it returns "true" if, it finds an element for which provided function returns true 
+// it returns "true" if, it finds an element for which provided function returns true
+//otherwise it returns "false", it doesn't modify array.
+
+const ages = [3, 10, 18, 20];
+ages.some(checkAdult)
+
+function checkAdult(age){
+    
+}
