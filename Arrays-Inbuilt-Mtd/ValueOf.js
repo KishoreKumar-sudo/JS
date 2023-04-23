ec77364baf435d0c45fda95a71f10fbefae0b959
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-23 20:57:13 +0530

    6

--- Arrays-Inbuilt-Mtd/ValueOf.js
+++ Arrays-Inbuilt-Mtd/ValueOf.js
@@ -1,3 +1,8 @@
 // syntax => array.valueOf()
 
-//valueOf() returns the array itself & dz mtd
+//valueOf() returns the array itself & dz mtd doesn't change the original array
+// fruits.valueOf() returns the same as fruits.i?
+
+//get the value of fruits:
+
+const fruits=[]
