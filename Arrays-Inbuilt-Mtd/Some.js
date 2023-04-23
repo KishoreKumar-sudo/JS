0e6c6bd302a037b6d458167a1c9f44000a4900c6
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-23 20:43:54 +0530

    2

--- Arrays-Inbuilt-Mtd/Some.js
+++ Arrays-Inbuilt-Mtd/Some.js
@@ -1,2 +1,4 @@
 //syntax => array.some(function(value, index, arr),this)
-//some() tests whether atleast one element in the array passes test  
+
+//some() tests whether atleast one element in the array passes test implemented by provided function
+// it returns "true" if, it finds an element for which provided function returns true 
