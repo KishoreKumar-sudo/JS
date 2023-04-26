bc3fba325128e32ca172de38d1c9565f7da17231
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 22:57:45 +0530

    7

--- Iterative Flow/ForIn.js
+++ Iterative Flow/ForIn.js
@@ -1,5 +1,14 @@
 //for..in loop is used to iterate over the properties of an object 
 
+//syntax
 /* for (variableName in object){
     Block of code
 } */
+
+const obj = { a: 1, b: 2, c: 3 };
+for (let prop in obj) {
+    console.log(`${prop}:${obj[prop]}`);
+}
+/* a:1
+b:2
+c:3 */
