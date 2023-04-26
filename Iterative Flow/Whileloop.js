c8d6e204e4c3c56efbf97e99d67978af3a753afa
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 23:02:45 +0530

    8

--- Iterative Flow/Whileloop.js
+++ Iterative Flow/Whileloop.js
@@ -1,9 +1,12 @@
+//while statement generates a loop that gets executed over a particular block of statement(code) as long as the condition is true.
+//every tym before executing the block of code the condition gets checked.
+
 //(i=100) Initialization,
 //(i>=50)Condition, 
 //(i=i+2 i=i-2 (or) i++,i--)Increment/Decrement 
 
-let i=100
-while (i>50) {
-    i=i-2
+let i = 100
+while (i > 50) {
+    i = i - 2
     console.log(i)
 }
