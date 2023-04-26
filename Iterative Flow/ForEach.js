c9316f3dd37db643cea39ccbbc3dcd0e27771be8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 22:42:09 +0530

    4

--- Iterative Flow/ForEach.js
+++ Iterative Flow/ForEach.js
@@ -1,2 +1,9 @@
+//forEach() is used to iterate over the elements of an array
+
 const numArray = [1, 2, 3]
-numArray.forEach()
+numArray.forEach(value => {
+    console.log(value);
+})
+/* 1
+2
+3 */
