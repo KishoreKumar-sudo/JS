12f4657b3ce90afdfbc7ca703fee806dae2659cd
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 22:46:29 +0530

    5

--- Iterative Flow/Forloop.js
+++ Iterative Flow/Forloop.js
@@ -1,6 +1,13 @@
+// condition checking & increment or decrements of the looping variable can be done in the same line.
+//incase of for loop the declaration & initialization of looping variable, 
+
 //(i=0) Initialization,
 //(i<=25)Condition, 
 //(i=i+1 i=i-1 (or) i++,i--)Increment/Decrement 
+
+/* syntax => for([initialization];[condition];[final-expression]){
+    Block of code
+ } */
 
 for (let i = 0; i <= 25; i++) {
     console.log(i)
