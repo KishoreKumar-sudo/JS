bac2c707b2f02c868bd2fcdff1320209c4a82b0d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-26 22:52:12 +0530

    6

--- Iterative Flow/DoWhileloop.js
+++ Iterative Flow/DoWhileloop.js
@@ -1,7 +1,10 @@
+// the do-while loop is similar to while loop, but it executes the block of code @least once, even if the condition is false
+
 //do something while condition is false (Initialization, Increment/Decrement, Condition)
 
-let i=1;
-do{
+let i = 1;
+
+do {
     console.log(i);
-    i++
-}while(i<=10)
+    i++;
+} while (i <= 10)
---
+++ Iterative Flow/ForIn.js
@@ -0,0 +1,5 @@
+//for..in loop is used to iterate over the properties of an object 
+
+/* for (variableName in object){
+    Block of code
+} */
