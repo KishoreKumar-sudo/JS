24cd2f036cb60403ae117add6e0843056d1e1d59
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-29 22:57:31 +0530

    3

--- JS-OOPS/FirstClass.js
+++ JS-OOPS/FirstClass.js
@@ -1,2 +1,9 @@
 //On this slide we are creating our-first class.
 //Remember to add the constructor that will enable object creation of instances(objects) based on our class
+
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
