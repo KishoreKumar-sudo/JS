b09ed871df7f3eadefa6630c76a51804f650b33f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-29 22:56:19 +0530

    2

--- JS-OOPS/FirstClass.js
+++ JS-OOPS/FirstClass.js
@@ -1,1 +1,2 @@
-//On this slide we are creating our-first class.   
+//On this slide we are creating our-first class.
+//Remember to add the constructor that will enable object creation of instances(objects) based on our class
