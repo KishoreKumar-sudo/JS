d2d439165e9b6f2f911a0d4791a9db0971103526
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-30 23:43:26 +0530

    4

--- JS-CH/ProperVariable.js
+++ JS-CH/ProperVariable.js
@@ -1,3 +1,9 @@
 const firstName = "John"
 const lastName = "Smith"
 const videos = assest.filter(assets => assets.type === "video")
+
+console.log(firstName)
+console.log(lastName)
+console.log(videos)
+
+
