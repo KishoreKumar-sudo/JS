d21956263d46694ec58a06bb77b6fb3183c4b1b5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-02 22:51:12 +0530

    1

--- JS-CH/ArrayOfObject.js
+++ JS-CH/ArrayOfObject.js
@@ -1,7 +1,15 @@
+// Unsolved 1
+
+const tickets = []
+
 let user1 = { name: 'JS' }
 let user2 = { name: 'TS' }
 
 tickets.push(user1)
 tickets.push(user2)
 
-user2 = user1
+user2 === user1
+
+console.log(user2);
+
+// Is tickets[1]===user2?
