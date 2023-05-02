9425c9c8a49662dd1d8d9be8aff102a05b6b6e46
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-02 23:11:17 +0530

    3

--- Arrays-Inbuilt-Mtd/ForEach.js
+++ Arrays-Inbuilt-Mtd/ForEach.js
@@ -1,3 +1,8 @@
 // syntax => array.forEach(function(currentValue, index, arr), thisValue)
 
-//forEach() executes a provided function once for each array element 
+//forEach() executes a provided function once for each array element & dz mtd is not executed for empty elements. 
+
+//Multiply each element:
+const numbers = [65, 44, 12, 4]
+numbers.forEach(myFunction)
+    
