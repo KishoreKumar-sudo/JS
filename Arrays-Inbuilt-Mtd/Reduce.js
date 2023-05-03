4b93b0a18cd1f5e07fb01e92988fc32e83d4a963
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-03 23:44:25 +0530

    3

--- Arrays-Inbuilt-Mtd/Reduce.js
+++ Arrays-Inbuilt-Mtd/Reduce.js
@@ -1,3 +1,4 @@
-//reduce() mtd executes a reducer function for array element & dz mtd returns a single value
+//reduce() mtd executes a reducer function for array element, dz mtd returns a single value & the function's accumulated result.
 
 //syntax => array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+
