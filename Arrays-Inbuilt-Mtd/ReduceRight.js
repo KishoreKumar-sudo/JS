4933ab04f403894491d220300a00311920ff7584
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-04 23:38:56 +0530

    6

--- Arrays-Inbuilt-Mtd/ReduceRight.js
+++ Arrays-Inbuilt-Mtd/ReduceRight.js
@@ -3,4 +3,12 @@
 //reduce() executes a reducer function for array element & dz mtd returns a single value, function's accumulated result.
 //works from right to left
 
+const numbers = [175, 50, 25]
 
+numbers.reduceRight(myFunc)
+
+function myFunc(total, num) {
+    return total - num
+}
+
+console.log(numbers.reduceRight(myFunc)) //-200
