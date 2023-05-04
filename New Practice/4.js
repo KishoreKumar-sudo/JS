723db86e361e2225f6a99f83c17fb568d2562733
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-04 23:29:50 +0530

    4

--- New Practice/4.js
+++ New Practice/4.js
@@ -1,3 +1,6 @@
+//variables declared with the 'let' keyword are block-scoped, so even though 'a' is re-initialized to 20 within the if statement,
+//that value of 'a' is only visible with if statement's block scope & console is in outer scope therefore it can be access 'a' variable in outer scope. 
+
 let a = 10
 if (a === 10) {
     let a = 20
