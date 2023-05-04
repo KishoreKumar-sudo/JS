45603bd3fda538079339eae1d24c617cfcd3b124
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-04 23:35:27 +0530

    5

---
+++ Arrays-Inbuilt-Mtd/ReduceRight.js
@@ -0,0 +1,6 @@
+// syntax => array.reduceRight(function(total, currentValue, currentIndex, arr), initialValue)
+
+//reduce() executes a reducer function for array element & dz mtd returns a single value, function's accumulated result.
+//works from right to left
+
+
