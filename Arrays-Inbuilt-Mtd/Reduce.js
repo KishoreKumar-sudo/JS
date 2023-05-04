eef63008cf5c62f916e363c45289750946fd1f6a
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-04 23:16:49 +0530

    1

--- Arrays-Inbuilt-Mtd/Reduce.js
+++ Arrays-Inbuilt-Mtd/Reduce.js
@@ -2,11 +2,13 @@
 
 //syntax => array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
 
-const numbers = [175, 50, 25]
+const number = [175, 50, 25]
+const numbers = [175, 50, 50]
 numbers.reduce(myFunc)
 
 function myFunc(total, num) {
     return total - num
 }
 
-console.log(numbers.reduce(myFunc))
+console.log(number.reduce(myFunc)) 
+console.log(numbers.reduce(myFunc)) //75
