977acf33ca09611e47e62929576a42aa0af94651
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-05 23:52:10 +0530

    6

--- Arrays-Inbuilt-Mtd/From.js
+++ Arrays-Inbuilt-Mtd/From.js
@@ -1,7 +1,9 @@
 // syntax => Array.from(object, mapFunction, thisValue);
 
-//Array.from() returns an array from any object with a length property & returns  an array from any iterable object.
+//Array.from() returns an array from any object with a length property
+// dz mtd returns an array from any iterable object.
 
-//create an array from an string
+//create an array from an string.
 
 console.log(Array.from("Code"));
+
