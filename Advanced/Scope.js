cc09ad886556a4784b607cec3f85faddc8eeaecb
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-07 22:23:00 +0530

    5

--- Advanced/Scope.js
+++ Advanced/Scope.js
@@ -1,22 +1,22 @@
 /* 'Global Scope' */
-const PIE=3.14;
+const PIE = 3.14;
 
-function foo(){
+function foo() {
     console.log(PIE);
 
     /* function scope */
-    const age=32;
+    const age = 32;
     console.log(age);
-} 
+}
 
 /* Block Scope */
 if (true) {
-    const fullName='Kishore Kumar';
+    const fullName = 'Kishore Kumar';
     console.log(fullName);//Kishore Kumar
 }
 
-console.log(PIE);
+console.log(PIE); //3.14
 
-console.log(age);
+console.log(age); //ReferenceError: age is not defined
 
-console.log(fullName);
+console.log(fullName); //ReferenceError: fullname is not defined
