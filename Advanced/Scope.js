51d7f851587387bc09951062c85eb1c8f98009a3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-07 22:19:19 +0530

    3

--- Advanced/Scope.js
+++ Advanced/Scope.js
@@ -1,10 +1,16 @@
 /* 'Global Scope' */
-const PIE=3.14
+const PIE=3.14;
 
 function foo(){
     console.log(PIE);
 
     /* function scope */
-    const age=32
+    const age=32;
     console.log(age);
 } 
+
+/* Block Scope */
+if (true) {
+    const fullName='Kishore Kumar';
+    console.log(fullName);//Kishore Kumar
+}
