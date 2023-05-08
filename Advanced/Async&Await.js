bc8414b53d9bdb6bafcb6dd17c51e2c50c5ace8d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-08 22:57:36 +0530

    4

--- Advanced/Async&Await.js
+++ Advanced/Async&Await.js
@@ -1,9 +1,10 @@
 //used async to make function act asynchronous
-async function getWeatherData(){
+async function getWeatherData() {
     try {
         //used await to make the code wait until promise returns a result
-        const res=await fetch('https://placeholder')
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json()
     } catch (error) {
-        
+
     }
 }
