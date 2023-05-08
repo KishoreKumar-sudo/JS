a838c1fd2b5274c0ddb3229d4e4aea1351e57885
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-08 22:47:59 +0530

    2

--- Advanced/Async&Await.js
+++ Advanced/Async&Await.js
@@ -1,4 +1,8 @@
 //used async to make function act asynchronous
 async function getWeatherData(){
-    
+    try {
+        //used await to make the code wait
+    } catch (error) {
+        
+    }
 }
