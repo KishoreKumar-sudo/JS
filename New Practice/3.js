7c98a40670e060250bd7c58b2f98c8ae1f2b074f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-09 23:44:14 +0530

    7

--- New Practice/3.js
+++ New Practice/3.js
@@ -2,9 +2,11 @@ let x = 10;
 let y = 20;
 console.log("total = " + x + y); //total = 1020
 
-console.log(NaN = NaN)
+console.log(NaN = NaN);
 
 for (let i = 1; i < 4; i++) {
-    if (i === 2) continue
-
+    if (i === 2) continue;
+    console.log(i);
 }
+/* 1
+   3 */
