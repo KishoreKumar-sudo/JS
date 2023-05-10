577873e576f4e0aa96006dd2c1065e65587c76b2
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-10 20:36:59 +0530

    4

--- Advanced/Date.js
+++ Advanced/Date.js
@@ -4,4 +4,12 @@ console.log(date); //2023-05-10T14:51:44.766Z
 console.log(date.getDate()); //10
 console.log(date.getMonth());
 console.log(date.getFullYear()); //2023
+console.log(date.getTime()); //1683731023769
 console.log(date.getHours()); //20
+console.log(date.getMinutes()); //32
+console.log(date.getSeconds()); //10
+console.log(date.getMilliseconds()); //200
+console.log(date.setDate(10));
+console.log(date.setMonth(5));
+console.log(date.setFullYear(2024));
+
