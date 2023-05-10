2fae673b515c63097bf2036c7f74ad010f67ff94
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-10 20:31:26 +0530

    3

--- Advanced/Date.js
+++ Advanced/Date.js
@@ -1,6 +1,7 @@
 const date = new Date();
-console.log(date) //2023-05-10T14:51:44.766Z
-console.log(date.getDate()) //10
-console.log(date.getMonth())
-console.log(date.getFullYear()) //2023
 
+console.log(date); //2023-05-10T14:51:44.766Z
+console.log(date.getDate()); //10
+console.log(date.getMonth());
+console.log(date.getFullYear()); //2023
+console.log(date.getHours()); //20
