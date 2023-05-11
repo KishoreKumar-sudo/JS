e9beb980603d5f96a4f99eeae16f94c26ecdac65
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-11 22:37:36 +0530

    3

--- Arrays-Inbuilt-Mtd/ArrayMtds.js
+++ Arrays-Inbuilt-Mtd/ArrayMtds.js
@@ -1,4 +1,9 @@
 const myArray = [1, 2, 3, 4]
 
 //Removes the first element(1) from the array & returns it.
-console.log(myArray.shift());  
+console.log(myArray.shift());  //1
+console.log(myArray); //[ 2, 3, 4 ]
+
+//Adds the element 0 to the beginning of the array & returns the new length
+console.log(myArray.unshift(0)) //4
+console.log(myArray) //[ 0, 2, 3, 4 ]
