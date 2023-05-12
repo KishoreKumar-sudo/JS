8313d29fdb202f9e39c62708da6709441069a425
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-12 23:49:17 +0530

    5

--- New Practice/6.js
+++ New Practice/6.js
@@ -1,5 +1,5 @@
-const removekey = () => {
+const removekey = (key, obj) => {
     const { [key]: omitted, ...res } = obj
     return res
 };
-console.log(removekey("bar", { foo: 1, bar: 2 }));
+console.log(removekey("bar", { foo: 1, bar: 2 })); //{ foo: 1 }
