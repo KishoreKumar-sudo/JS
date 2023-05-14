a8d2650595445cc28540ab2f01e6fdfa3d096c21
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-14 22:03:32 +0530

    3

--- Advanced/Array.js
+++ Advanced/Array.js
@@ -2,3 +2,6 @@ const fruits=['Apple','Banana','Grapes','Orange','Strawberry','Apple']
 
 //converts the array to a string
 console.log(fruits.toString()); //Apple,Banana,Grapes,Orange,Strawberry,Apple
+
+//adds element @ the end of the array
+console.log(fruits.push('Pineapple')); 
