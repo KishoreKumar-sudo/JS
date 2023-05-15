d85a619bbd229ba90c8fc061dd5be5464c4e9ca9
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-15 23:26:40 +0530

    6

--- Advanced/Array.js
+++ Advanced/Array.js
@@ -1,11 +1,14 @@
-const fruits=['Apple','Banana','Grapes','Orange','Strawberry','Apple']
+const fruits = ['Apple', 'Banana', 'Grapes', 'Orange', 'Strawberry', 'Apple']
 
 //converts the array to a string
 console.log(fruits.toString()); //Apple,Banana,Grapes,Orange,Strawberry,Apple
 
 //adds element @ the end of array
-console.log(fruits.push('Pineapple')); 
+console.log(fruits.push('Pineapple'));
 console.log(fruits.toString()); //Apple,Banana,Grapes,Orange,Strawberry,Apple,Pineapple
 
 //removes the last element of array
 console.log(fruits.pop());//Pineapple
+
+//checks if array contains an element
+console.log(fruits.includes('Banana')); //true
