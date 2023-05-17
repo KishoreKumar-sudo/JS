6cfbf1f8b0a025e6f2e0d6eb50665c54663c05bc
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-17 22:56:47 +0530

    4

--- Data/2.js
+++ Data/2.js
@@ -4,6 +4,7 @@ let y = Object.assign({}, x)
 console.log(x === y);//false
 
 //in dz case we are comparing teo strings using (>=) operator, it performs lexicographic(dictionary) comparison based upon unicode values of character.
+//in dz case both a & b are strings with value '10', since both are strings are equal & comparison evaluates to be true.
 
 let a = '10'
 let b = '10'
