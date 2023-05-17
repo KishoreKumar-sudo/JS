bf7d42edb1b2148ac638509d12b4efda47fe39cf
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-17 23:06:13 +0530

    5

--- Data/2.js
+++ Data/2.js
@@ -5,9 +5,13 @@ console.log(x === y);//false
 
 //in dz case we are comparing teo strings using (>=) operator, it performs lexicographic(dictionary) comparison based upon unicode values of character.
 //in dz case both a & b are strings with value '10', since both are strings are equal & comparison evaluates to be true.
-
-let a = '10'
-let b = '10'
+let a = '10';
+let b = '10';
 
 console.log(a >= b); //true
 
+let c = 10;
+let d = 5;
+
+console.log(!(c < d)); //true !(10<5), c is less than d then d will return false
+//!(false) = true
