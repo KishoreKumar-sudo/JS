e274618b9aea897869fd1f404084a6edd7c8c841
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-20 23:44:34 +0530

    2

--- Advanced/Entries.js
+++ Advanced/Entries.js
@@ -1,1 +1,3 @@
-// returns an Array iterator object with key/value pairs, it doesn't change original array.
+// entries() returns an Array iterator object with key/value pairs, it doesn't change original array.
+
+const cars=["Toyota","Maruti Suzuki","Hyndai","Tata"]
