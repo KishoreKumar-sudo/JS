f10ce9a6d527e7c0c458eb25c09f53f6ebd43545
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-20 23:49:10 +0530

    3

--- Advanced/Entries.js
+++ Advanced/Entries.js
@@ -1,3 +1,5 @@
 // entries() returns an Array iterator object with key/value pairs, it doesn't change original array.
 
-const cars=["Toyota","Maruti Suzuki","Hyndai","Tata"]
+const cars = ["Toyota", "Maruti Suzuki", "Hyndai", "Tata"];
+const c = cars.entries;
+console.log(cars.entries()); //Object [Array Iterator] {}
