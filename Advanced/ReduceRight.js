e1c341410257fc2d34abcb2123d054b3c512479c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-21 22:19:14 +0530

    2

--- Advanced/ReduceRight.js
+++ Advanced/ReduceRight.js
@@ -1,1 +1,6 @@
-//reduceRight() executes a reducer function for array element.
+//reduceRight() mtd executes a reducer function for array element & it works from right to left.
+// it returns a single value
+
+const array1 = [[0, 1], [2, 3], [4, 5]]
+
+const result=array1.reduceRight()
