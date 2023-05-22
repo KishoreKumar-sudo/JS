c8eb7886805e8e9e14df806fec94e1ac7b502743
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-22 22:35:37 +0530

    4

--- Advanced/Join.js
+++ Advanced/Join.js
@@ -1,5 +1,5 @@
 //join() mtd doesn't change original array, any separator can be specified default is comma(,).
 // returns an Array as a string.
 
-const elements =['Fire', 'Air', 'Water'];
-console.log(elements.join());
+const elements = ['Fire', 'Air', 'Water'];
+console.log(elements.join()); //Fire,Air,Water
