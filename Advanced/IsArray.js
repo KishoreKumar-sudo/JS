1125c85486829894eeab66cf1bdbf20afe91703c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-23 22:45:19 +0530

    3

--- Advanced/IsArray.js
+++ Advanced/IsArray.js
@@ -1,3 +1,6 @@
 //Array is a keyword, u can't put anyother user-defined array.
 //returns true if an object is an array, otherwise false
 
+const arr=["Day","Night","Evening"]
+console.log(Array.isArray(arr));
+
