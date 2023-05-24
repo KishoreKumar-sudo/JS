89d47b50dac3f7b498732fe587c5a73ec0a51303
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-24 22:44:07 +0530

    2

--- Fun-Facts/1.js
+++ Fun-Facts/1.js
@@ -1,1 +1,8 @@
-//the new at () mtd for arrays allows you to access from either end easily.
+//the new at() mtd for arrays allows you to access from either end easily.
+
+const arr=['a','b','c','d']
+
+console.log(arr.at(0))
+console.log(arr.at(1))
+console.log(arr.at(-1))
+console.log(arr.at(0))
