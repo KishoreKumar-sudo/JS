b18e86882e2a480c0da73246ec26d5db5827f31c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-25 22:26:36 +0530

    3

--- Fun-Facts/2.js
+++ Fun-Facts/2.js
@@ -1,3 +1,5 @@
-const foo=new Date('May 25, 2023 00:00:00');
-foo.setDate(foo.getDate()+1);
-console.log(foo); 
+//the Date object is smart enough to automatically adjust any overflows 
+
+const foo = new Date('May 25, 2023 00:00:00');
+foo.setDate(foo.getDate() + 1);
+console.log(foo);  //2023-05-25 
