67211c9ac8ceab84c13a0f98612c98268de8634e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-27 22:09:56 +0530

    4

--- Fun-Facts/3.js
+++ Fun-Facts/3.js
@@ -1,11 +1,10 @@
 //Underscore can be used as separator for numbers.
-//dz z useful for large values to improve readability.
-
+//dz will useful for large values to improve readability.
 const largeNum = 1_000_000;
 
 console.log(largeNum);//1000000
 
-//Since strings are iterables,
+//Since strings are iterables, they are split into array of characters by using spread operator.
 const arr = [...'hello'];
 
 console.log(arr); //[ 'h', 'e', 'l', 'l', 'o' ]
