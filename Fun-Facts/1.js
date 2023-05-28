dfbf3e1f2dda4e0df7e41317b251be75d15c13f3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-28 22:45:21 +0530

    3

--- Fun-Facts/1.js
+++ Fun-Facts/1.js
@@ -8,7 +8,8 @@ console.log(arr.at(2)) //c
 console.log(arr.at(3)) //d
 
 //
-const start=performance.now() 
-// cost duration=performance
+const start = performance.now()
+const duration = performance.now() - start
 
 console.log(start); //138.20899987220764
+console.log(duration); //0.035199880599975586
