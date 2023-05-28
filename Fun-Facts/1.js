fa568c1fb5a1ff694ec07c87b431a06b7facb2a8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-28 22:48:06 +0530

    5

--- Fun-Facts/1.js
+++ Fun-Facts/1.js
@@ -7,9 +7,9 @@ console.log(arr.at(1)) //b
 console.log(arr.at(2)) //c
 console.log(arr.at(3)) //d
 
-//
+//the performance API can be used to calculate time b/w two events precisely (useful for benchmarking).
 const start = performance.now()
 const duration = performance.now() - start
 
-console.log(start); //138.20899987220764
-console.log(duration); //0.035199880599975586(precise time duration)
+console.log(start); //115.478600025177
+console.log(duration); //0.03410005569458008(precise time duration)
