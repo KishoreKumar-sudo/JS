67235f6d39e9284761e2429a991d772a804c3629
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-29 23:30:31 +0530

    3

--- Fun-Facts/2.js
+++ Fun-Facts/2.js
@@ -5,4 +5,8 @@ const foo = new Date('May 25, 2023 00:00:00');
 foo.setDate(foo.getDate() + 1);
 console.log(foo);  //2023-05-25 
 
-//the classic for loop can be prefixed with await to iterate over an array of promises, but wait for each promise to resolve before moving to the next
+//the classic for loop can be prefixed with await to iterate over an array of promises, but wait for each promise to resolve before moving to the next.
+
+async function test(){
+    const promises =[/*  */]
+}
