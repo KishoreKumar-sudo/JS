9f9bd673bddc4d37f33dbc40d3b8cfffd7bc2963
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-30 20:13:07 +0530

    'done'

--- Fun-Facts/4.js
+++ Fun-Facts/4.js
@@ -1,6 +1,6 @@
 //the following syntax provides both the index & value when iterating over an array
 
-const arr = ['foo', 'bar', 'baz']
+const arr = ['foo', 'bar', 'baz'];
 
 for (const [key, value] of arr.entries()) {
     console.log(key, value);
