3ef03f474a73638ec70d7749403358ae2cf4c59d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-02 13:11:24 +0530

    2

--- New Practice/1.js
+++ New Practice/1.js
@@ -3,8 +3,11 @@
 
 console.log("A" - "B" + "2")
 
+//replace() finds the first occurence of substring "Hell" within the string "Hello World", replaces it with "G" o/p as "Go World",
+// modified string is assigned to variable y, then find the length of the string.
+//(7 Characters + 1 empty space = 8 )
 let x = "Hello World";
 let y = x.replace("Hell", "G");
 
-console.log(y);
-console.log(y.length);
+console.log(y); //Go World
+console.log(y.length); //8
