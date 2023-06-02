85a8fb5cf2849ddce958902075e0c321344885e1
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-02 11:35:28 +0530

    1

--- New Practice/1.js
+++ New Practice/1.js
@@ -2,3 +2,9 @@
 //when we concatenate "2" to NaN using the + operator, JS will convert NaN to a string & concatenate it with "2", resulting into string of "NaN2"  
 
 console.log("A" - "B" + "2")
+
+let x = "Hello World";
+let y = x.replace("Hell", "G");
+
+console.log(y);
+console.log(y.length);
