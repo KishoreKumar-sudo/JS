ff2ffe14cd9cfbe249a98664838f134bececcf03
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-05 22:44:00 +0530

    3

--- Object-Method/Values.js
+++ Object-Method/Values.js
@@ -1,6 +1,10 @@
-const employee ={
-    name:"KishoreKumar"
-    age:26
-    occupation:"Mechanical Engineer"
-    level:2
+//Iterates over the object & returns the object's values.
+
+const employee = {
+    name: "KishoreKumar",
+    age: 26,
+    occupation: "Mechanical Engineer",
+    level: 2
 }
+
+console.log(Object.);
