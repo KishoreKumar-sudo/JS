89b28244190f400f320d9eb2aefd91cbf863f146
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-06 22:21:22 +0530

    5

--- Object-Method/Seal.js
+++ Object-Method/Seal.js
@@ -1,12 +1,13 @@
 //Seals an object which prevents new properties from being added to it,
 //marks all existing properties as non-configurable.
 
-const car={price:12000};
+const car = { price: 12000 };
 Object.seal(car);
 
-car.price=15000; //value changed successfully
+car.price = 15000; //value changed successfully
 
-console.log(car.price);
+console.log(car.price); //15000
 
 delete car.price;
-console.log(car);
+
+console.log(car.price);
