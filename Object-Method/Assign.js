4d291588c0cc3d2ad520f7ab0cca0a8c32d8c36e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-07 23:06:55 +0530

    5

--- Object-Method/Assign.js
+++ Object-Method/Assign.js
@@ -1,4 +1,7 @@
 //Copies all enumberable & own properties from the source objects to the target object.
 //It returns the target object & also called shallow copy.
+
 const target = { a: 1, b: 2 };
-const source = { b: 4, c: 5 }
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
