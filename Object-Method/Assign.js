b8b8807663d00e0099cb1d67833dae43d374f718
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-07 23:04:11 +0530

    3

--- Object-Method/Assign.js
+++ Object-Method/Assign.js
@@ -1,3 +1,4 @@
+//Copies all enumberable & own properties from the source objects to the target object.
 //
-const target={a:1,b:2};
-const source={b:4,c:5}
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 }
