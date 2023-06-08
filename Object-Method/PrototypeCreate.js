bd5a9d1a92d2a359b7fcb49b56ec171827b98eef
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-08 22:32:10 +0530

    5

--- Object-Method/PrototypeCreate.js
+++ Object-Method/PrototypeCreate.js
@@ -1,6 +1,10 @@
 //Creates a new object, using an existing object as prototype of the newly created object.
 
-let student = {name:"fuzzy",
-display(){
-    console.log("Name:", this.name);
-}};
+let student = {
+    name: "fuzzy",
+    display() {
+        console.log("Name:", this.name);
+    }
+};
+
+let std1 = Object.create(student)
