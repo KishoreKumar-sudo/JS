8812d2e3e93a4d77899483a9eb568ee886e2872f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-09 15:39:25 +0530

    11

--- Data/1.js
+++ Data/1.js
@@ -8,6 +8,6 @@ myVariable="Kishore"
 console.log(myVariable)
 console.log(typeof(myVariable))
  
-myVariable = 42
-console.log(myVariable)
-console.log(typeof(myVariable))
+myVariable = 42;
+console.log(myVariable);
+console.log(typeof(myVariable));
