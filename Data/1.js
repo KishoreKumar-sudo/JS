ed09ca0c2988ca4c5389ec80633e28c5d4d7c263
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-09 15:39:43 +0530

    12

--- Data/1.js
+++ Data/1.js
@@ -1,12 +1,12 @@
 let myVariable;
 // typeof(myVariable) (@ browser-level u will get undefined)
 
-console.log(myVariable)
+console.log(myVariable);
 console.log(typeof(myVariable));   
 
-myVariable="Kishore"
-console.log(myVariable)
-console.log(typeof(myVariable))
+myVariable="Kishore";
+console.log(myVariable);
+console.log(typeof(myVariable));
  
 myVariable = 42;
 console.log(myVariable);
