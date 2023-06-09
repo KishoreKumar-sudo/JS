9c6a992fb47a8d26d1db03737b07c836c45cd87b
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-09 15:33:53 +0530

    4

--- Object-Method/Freeze.js
+++ Object-Method/Freeze.js
@@ -3,5 +3,6 @@
 /* 1.New Properties from being added to the object.
    2.Existing properties to be removed from object.
    3.Changing the enumberability, configurability or writablity of existing properties.
+   4.Changing values of the existing properties & prototype.
 */
 
