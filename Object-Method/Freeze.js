7ffc3faea438877b71665beea1c1971f4b592327
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-09 15:32:57 +0530

    3

--- Object-Method/Freeze.js
+++ Object-Method/Freeze.js
@@ -1,1 +1,7 @@
-//Freezes an object, a frozen object can no longer be changed.
+//Freezes an object, a frozen object can no longer be changed this means:
+
+/* 1.New Properties from being added to the object.
+   2.Existing properties to be removed from object.
+   3.Changing the enumberability, configurability or writablity of existing properties.
+*/
+
