14539e2ae7695e0df7e8a60dc8b1464d10f8c352
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-13 22:32:25 +0530

    3

--- Fun-Facts/Console-In-JS/Ex1.js
+++ Fun-Facts/Console-In-JS/Ex1.js
@@ -1,15 +1,16 @@
 //Clear(), clears the terminal without any hesitation. 
 let tips = "PushpaRise"
 console.log(tips); //PushpaRise
-// console.clear();
+console.clear();
 
 //Info()
 let tip = "PushpaRule"
-console.info(tip) //PushpaRule
+console.info(tip); //PushpaRule
 
 //error()
 let name = "PushpaRise"
-console.error(name, 'This is a simple error');
+console.error(name, 'This is a simple error'); //PushpaRise This is a simple error
 
 //log()
 let tipz = "PushpaRule"
+console.log(tipz); //PushpaRule
