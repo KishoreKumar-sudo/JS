f0b0bbd4c904fdc8bc0afcb205838e66f04c105d
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-19 22:44:00 +0530

    2

--- New Practice/2.js
+++ New Practice/2.js
@@ -3,3 +3,4 @@ let y = x.join("");
 console.log(typeof y); //string
 
 console.log([1,2] +[3,4]); //1,23,4
+//the arrays are converted to strings '1,2' & '3,4' and they joined end to end ('1,23,4') 
