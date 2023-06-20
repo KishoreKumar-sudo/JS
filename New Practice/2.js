77bb381ca7a67e91590f00457b1ec57c7aa18fd2
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-20 19:20:03 +0530

    2

--- New Practice/2.js
+++ New Practice/2.js
@@ -5,8 +5,14 @@ console.log(typeof y); //string
 //the arrays are converted to strings '1,2' & '3,4' and they joined end to end ('1,23,4')
 console.log([1, 2] + [3, 4]); //1,23,4
 
-function Person(name){
+function Person(name) {
     this.name;
 }
-const john =new Person("John");
+
+const john = new Person("John");
 console.log(john.name); //undefined
+
+let n = [1, 2, 3, 4, 5]
+let p = n.every((x) => x > 0);
+
+console.log(p); //true
