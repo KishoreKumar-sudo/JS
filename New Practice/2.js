30fee248ff5cdec463321affea89073a7e039d56
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-20 19:16:31 +0530

    1

--- New Practice/2.js
+++ New Practice/2.js
@@ -2,5 +2,11 @@ let x = [1, 2, 3];
 let y = x.join("");
 console.log(typeof y); //string
 
-console.log([1, 2] + [3, 4]); //1,23,4
 //the arrays are converted to strings '1,2' & '3,4' and they joined end to end ('1,23,4')
+console.log([1, 2] + [3, 4]); //1,23,4
+
+function Person(name){
+    this.name;
+}
+const john =new Person("John");
+console.log(john.name); //undefined
