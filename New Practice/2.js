362f1c73cc17ad3c2f6406bae3450d18a604bc68
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-20 19:24:25 +0530

    3

--- New Practice/2.js
+++ New Practice/2.js
@@ -12,6 +12,8 @@ function Person(name) {
 const john = new Person("John");
 console.log(john.name); //undefined
 
+//Bcz every element of n are greater than 0 and when all of the condition satisfies with "every" function, it returns true.
+//If only one of the conditions were not satisfied then "false" would have been consoled. 
 let n = [1, 2, 3, 4, 5]
 let p = n.every((x) => x > 0);
 
