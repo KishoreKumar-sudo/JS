71415950aad8dca0b661e612ec7985c51f449a60
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-27 23:11:03 +0530

    1

--- Fun-Facts/Console-In-JS/Ex1.js
+++ Fun-Facts/Console-In-JS/Ex1.js
@@ -1,9 +1,9 @@
-//Clear(), clears the terminal without any hesitation. 
+//clear(), clears the terminal without any hesitation. 
 let tips = "PushpaRise"
 console.log(tips); //PushpaRise
 console.clear();
 
-//Info()
+//info()
 let tip = "PushpaRule"
 console.info(tip); //PushpaRule
 
@@ -14,3 +14,13 @@ console.error(name, 'This is a simple error'); //PushpaRise This is a simple err
 //log()
 let tipz = "PushpaRule"
 console.log(tipz); //PushpaRule
+
+//warn()
+let method = "This is a warning!";
+console.warn(method);
+
+//table() => Array
+console.table(["HTML", "CSS", "JAVASCRIPT"]);
+
+//table() => Object
+({fName:"Kishore",lName:"Kumar"});
