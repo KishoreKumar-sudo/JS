4e3da0220301dd16d2a5270618308c0f3dddbfc8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-09-18 20:46:00 +0530

    3

--- JS-CH/ProperVariable.js
+++ JS-CH/ProperVariable.js
@@ -4,13 +4,13 @@ const firstName = "John"
 const lastName = "Smith"
 const videos = asserts.filter(asserts => asserts.type === "video")
 
-console.log(firstName)
-console.log(lastName)
+console.log(firstName) //John
+console.log(lastName) //Smith
 console.log(videos)
 
 //how to generate compact numbers?
-const formatter =Intl.NumberFormat('en',{notation:'compact'});
-formatter.format(123); 
-formatter.format(1234); 
-formatter.format(1235678);
-formatter.format(1235678901);
+const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+console.log(formatter.format(123)); //123
+console.log(formatter.format(1234)); //1.2K
+console.log(formatter.format(1235678)); //1.2M
+console.log(formatter.format(1235678901)); //1.2B
