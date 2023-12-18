197748b344b429d45d42304be34f19718b333ddb
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-12-18 23:09:23 +0530

    1

---
+++ ES6/Delete.js
@@ -0,0 +1,7 @@
+// Deleting Array Element Based on Object Property in JavaScript:
+let arrayOfObjects = [
+    { id: 1, name: 'Kishore' },
+    { id: 2, name: 'Vijay' },
+    { id: 3, name: 'Kamesh' }
+]
+//Delete Object with id 2
