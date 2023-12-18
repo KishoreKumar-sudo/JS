57c0a04ceeaf77af325b31861978f8327e2964b6
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-12-18 23:14:27 +0530

    2

--- ES6/Delete.js
+++ ES6/Delete.js
@@ -2,6 +2,15 @@
 let arrayOfObjects = [
     { id: 1, name: 'Kishore' },
     { id: 2, name: 'Vijay' },
-    { id: 3, name: 'Kamesh' }
+    { id: 3, name: 'John' },
+    { id: 4, name: 'Kamesh' }
 ]
-//Delete Object with id 2
+//Delete Object with id 3
+const objectIdToDelete = 3;
+arrayOfObjects = arrayOfObjects.filter(obj => obj.id !== objectIdToDelete)
+console.log(arrayOfObjects); 
+/*  [
+  { id: 1, name: 'Kishore' },
+  { id: 2, name: 'Vijay' },
+  { id: 4, name: 'Kamesh' }
+] */
