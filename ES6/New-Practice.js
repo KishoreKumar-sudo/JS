57596748fad68fbc053aa234e65c203e216a4746
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-12-19 22:58:41 +0530

    1

--- ES6/New-Practice.js
+++ ES6/New-Practice.js
@@ -1,7 +1,10 @@
-    //Arrow Functions
+//Arrow Functions provide a concise syntax for writing functions,especially useful fr short, one-line operations.
+
 //ES5 function
 function addition(a,b){
     return a+b;
 }
 //ES6 arrow function
 const addition =(a,b)=>a+b;
+
+//Template Literals allow embedding expressions inside strings,providing a cleaner & more readable way to concatenate strings
