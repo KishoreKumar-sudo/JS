5ba6469ed16d454a4d1a79cce5f96f7bf407145b
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-12-20 23:33:12 +0530

    2

--- ES6/New-Practice.js
+++ ES6/New-Practice.js
@@ -1,19 +1,4 @@
-//Arrow Functions provide a concise syntax for writing functions,especially useful fr short, one-line operations.
 
-// ES5 function
-function addition(a, b) {
-    return a + b;
-}
-// ES6 arrow function
-// const addition = (a, b) => a + b;
 
-// Template Literals 
-// Allows embedding expressions inside strings,providing a cleaner & more readable way to concatenate strings
-const name = 'Jack';
-const greeting = `Hello, ${name}!`;
 
-//console.log(greeting); //Hello, Jack!
 
-// Destructing Assignment
-//Simplifies the extraction of values from objects or arrays into individual variables.
-const person={name:'Atlee', age:37};
